e409e9d8630e96b8bb2110f37eae72ee3fe1797a
Chvryy01 <jhosepcv01@gmail.com>
2022-05-30 08:38:10 -0500

    respaldo01

---
+++ sesion19/misSripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function()){
+  $("a").click(function(evento)){
+    alert("Has pulsado el enlace. Ahora serás enviado a la página de la USAT")
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
